fba310e99b57e77fa4a9393a1261df53fe761306
heckenschwil <hugo.eckenschwiller@etudiant.univ-rennes1.fr>
2023-03-29 11:56:41 +0200

    change to fastAPI

--- backend/databaseUtils/apiFlask.py
+++ backend/databaseUtils/apiFlask.py
@@ -1,74 +1,57 @@
 import os
-from flask import *
-from flask_cors import CORS 
+from fastapi import FastAPI, Request
 import sqlite3  
 import json
 from datetime import datetime
 import Utils as dbUtils
+import uvicorn
 
 def create_utils():
     return dbUtils.Utils("bolt://localhost:7687", "neo4j", "bio4tdummy")
 
-def create_app():
+# def create_app():
 
-    app = Flask(__name__)
-    CORS(app)
+app = FastAPI()
+utils = create_utils()
+
+@app.post("/getLabels")
+
+async def getLabels(request: Request):
+    print(request)
+    text = request.get_json()["json"]["input"]
     utils = create_utils()
+    result = utils.request_topicsListWithFilter(text)
+    jsonResult = json.dumps(result)
+    print(jsonResult)
+    return jsonResult
 
+@app.post("/getInputs")
 
-    @app.route("/test", methods=['POST'])
+async def getInputs(request: Request):
+    print(request)
+    text = request.get_json()["json"]["input"]
+    print(str(text))
+    utils = create_utils()
+    result = utils.request_InputListWithFilter(text)
+    print(result)
+    jsonResult = json.dumps(result)
+    print()
+    return jsonResult
 
-    def test():
-        '''data = request.get_json()
-        print("La requête : "+data['json']['title'])
-        return data['json']['title']'''
-        context = request.get_json()["json"]
-        parameters = dbUtils.Param(input=context['input'], output=context['output'], limit=context['limit'], depth=context['depth'])
-        #connect.nodes[1234]
-        # print("La requête : ")
-        # print(builder.requete)
-        # print("Le résultat : ")
-        # print(json.dumps(result, indent=2))
-        return ""
-    
-    @app.route("/getLabels", methods=['POST'])
+@app.post("/getOutputs")
 
-    def getLabels():
-        print(request)
-        text = request.get_json()["json"]["input"]
-        utils = create_utils()
-        result = utils.request_topicsListWithFilter(text)
-        jsonResult = json.dumps(result)
-        print(jsonResult)
-        return jsonResult
-    
-    @app.route("/getInputs", methods=['POST'])
+async def getOutputs(request: Request):
+    print(request)
+    text = request.get_json()["json"]["input"]
+    utils = create_utils()
+    result = utils.request_OutputListWithFilter(text)
 
-    def getInputs():
-        print(request)
-        text = request.get_json()["json"]["input"]
-        print(str(text))
-        utils = create_utils()
-        result = utils.request_InputListWithFilter(text)
-        print(result)
-        jsonResult = json.dumps(result)
-        print()
-        return jsonResult
-    
-    @app.route("/getOutputs", methods=['POST'])
+    jsonResult = json.dumps(result)
+    print()
+    return jsonResult
 
-    def getOutputs():
-        print(request)
-        text = request.get_json()["json"]["input"]
-        utils = create_utils()
-        result = utils.request_OutputListWithFilter(text)
-
-        jsonResult = json.dumps(result)
-        print()
-        return jsonResult
-
-    return app 
+    # return app 
 
 if __name__ == "__main__":
-    app = create_app()  
-    app.run(host='0.0.0.0', port=5000)  
+    # app = create_app()  
+    uvicorn.run(app, host="0.0.0.0", port=5000)
